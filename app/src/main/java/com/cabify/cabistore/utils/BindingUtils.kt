0ff93540ba7408593de848c6408eda474b2c7c82
Diego Perez Salas <diegosalas@gmail.com>
2020-02-05 17:04:08 +0100

    EspressoText Checkout

--- app/src/main/java/com/cabify/cabistore/utils/BindingUtils.kt
+++ app/src/main/java/com/cabify/cabistore/utils/BindingUtils.kt
@@ -3,8 +3,11 @@ package com.cabify.cabistore.utils
 import android.annotation.SuppressLint
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 
 import com.cabify.cabistore.database.SaleDetail
+import com.cabify.cabistore.ui.cart.CartAdapter
+
 
 @BindingAdapter("setProductName")
 fun TextView.setProductName(item: SaleDetail?){
@@ -29,9 +32,24 @@ fun TextView.setProductPrice(item: SaleDetail?){
   }
 }
 
+@SuppressLint("SetTextI18n")
+@BindingAdapter("setProductSubTotal")
+fun TextView.setProductSubTotal(item: SaleDetail?){
+  item?.let{
+    var subTotal = item.price * item.quantity
+    text = "$subTotal €"
+  }
+}
+
 @BindingAdapter("setProductQty")
 fun TextView.setProductQty(item: SaleDetail?){
   item?.let{
     text = item.quantity.toString()
   }
+}
+
+@BindingAdapter("listData")
+fun bindRecyclerView(recyclerView: RecyclerView, data: List<SaleDetail>?) {
+  val adapter = recyclerView.adapter as CartAdapter
+  adapter.submitList(data)
 }
